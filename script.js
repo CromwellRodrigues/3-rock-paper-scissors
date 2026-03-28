04bc2a15633f2511e8ef4bb339d26ed184a1b101
daffodil009 <daffodil.luck9@gmail.com>
2026-03-28 11:01:28 +0000

    create human choice function

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ function getComputerChoice() {
 
 const computerPlay = getComputerChoice();
 console.log(computerPlay);
+
+
+function getHumanChoice() {
+    const humanChoice =prompt("Please choose rock, paper or scissors: ").toLowerCase();
+    
+    if (humanChoice === null) {
+        return ("Please enter a choice");
+    }
+
+    if (humanChoice === "") {
+        return ("Please type in a choice");
+    }
+
+    if (choices.includes(!humanChoice)) {
+        return ("Please enter a valid choice");
+    }
+
+    if (choices.includes(humanChoice)) {
+        return humanChoice;
+    }
+
+}
+
+
+const player = getHumanChoice();
+console.log(player);
